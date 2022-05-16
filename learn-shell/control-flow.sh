3143dec87b8e34c62e1071c0a5b19d16b4f1150f
haisheng.zheng <haisheng.zheng@klook.com>
2022-05-16 14:16:25 +0800

    learn shell, add files

---
+++ learn-shell/control-flow.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 学习 Shell 流程控制
+
+echo "学习 Shell 流程控制";
+
+EARTH_IS_ROUND=1
+
+if [ $EARTH_IS_ROUND == 1 ]
+then
+  echo "earth is round.";
+fi
---
+++ learn-shell/input.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 学习命令行读取用户输入
+
+echo "What's your name?";
+read name;
+echo "Hello, $name";
+
